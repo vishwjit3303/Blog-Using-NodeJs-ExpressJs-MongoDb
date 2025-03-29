a8bc775bb340dad4aec7eac05e0a9e998424b08d
VishwajeetPawargit config --global user.email vishwjitpawar340@gmail.com <vishwjitpawar340@gmail.com>
2025-03-29 20:34:14 +0530

    added

---
+++ 25.NodeJs-Express-EJS-MongoDB--Blog/app.js
@@ -0,0 +1,59 @@
+require('dotenv').config();
+const express = require('express');
+const expressLayout = require('express-ejs-layouts');
+const methodOverride = require('method-override');
+const cookieParser = require('cookie-parser');
+const session = require('express-session');
+const { MongoClient } = require('mongodb');
+const MongoStore = require('connect-mongo');
+
+console.log('MongoDB URI:', process.env.MONGODB_URI); // Log the connection string
+const client = new MongoClient(process.env.MONGODB_URI);
+
+const clientPromise = client.connect();
+
+
+const connectDB = require('./server/config/db');
+const { isActiveRoute } = require('./server/helpers/routeHelpers');
+
+const app = express();
+const PORT = process.env.PORT || 5000;
+  
+// Connect to DB
+connectDB();
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(cookieParser());
+app.use(methodOverride('_method'));
+
+app.use(session({
+  secret: 'keyboard cat',
+  resave: false,
+  saveUninitialized: true,
+  store: MongoStore.create({
+    clientPromise: clientPromise
+
+
+
+  }),
+  //cookie: { maxAge: new Date ( Date.now() + (3600000) ) } 
+}));
+
+app.use(express.static('public'));
+
+// Templating Engine
+app.use(expressLayout);
+app.set('layout', './layouts/main');
+app.set('view engine', 'ejs');
+
+
+app.locals.isActiveRoute = isActiveRoute; 
+
+
+app.use('/', require('./server/routes/main'));
+app.use('/', require('./server/routes/admin'));
+
+app.listen(PORT, ()=> {
+  console.log(`App listening on port ${PORT}`);
+});
